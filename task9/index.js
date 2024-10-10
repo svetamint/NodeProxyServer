824edde7a4a46f00aa429c4a2082e6f20f97dbef
s.lebiadzko <svetamint@gmail.com>
2024-10-10 12:20:29 +0200

    added parent & child views

--- task9/index.js
+++ task9/index.js
@@ -1,3 +1,5 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 import express from 'express';
 import {config} from "./config/config.js";
 import meteorRouter from './delivery/meteorController.js'
